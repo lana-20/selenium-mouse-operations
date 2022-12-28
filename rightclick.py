2af34d7529fd52c8404314bb40c1e8a054291723
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2022-12-28 10:50:25 -0800

    Update rightclick.py

--- rightclick.py
+++ rightclick.py
@@ -5,12 +5,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.import ActionChains
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html")
 
-...
+driver.implicitly_wait(10)
+driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html")
+driver.maximize_window()
+
+btn = driver.find_element(By.XPATH, "//span[@class='context-menu-one btn btn-neutral']")
 
 act = ActionChains(driver)
 
-...
+act.context_click(btn).perform()
 
 driver.close()
